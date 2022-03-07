4794892eb185366e88f6572bfa2836dee4c3baf2
pmaevsk <maevskips@gmail.com>
2022-03-07 18:02:04 +0300

    task_3

--- ht_3.py
+++ ht_3.py
@@ -3,18 +3,38 @@ from math import pi
 
 class Cylinder:
     @staticmethod
-    def make_area(d, h):
+    def __make_area(d, h):
         circle = pi * d ** 2 / 4
         side = pi * d * h
         return round(circle*2 + side, 2)
 
     def __init__(self, d, h):
-        self.d = d
-        self.h = h
-        self.area = self.make_area(d, h)
+        self.diam = d
+        self.high = h
+        self.__area = self.__make_area(d, h)
+
+    def __setattr__(self, attr, value):
+        if attr in ['diam', 'high', '_Cylinder__area']:
+            self.__dict__[attr] = value
+            if attr in ['diam', 'high']:
+                try:
+                    self.__dict__['_Cylinder__area'] = self.__make_area(
+                        self.diam, self.high)
+                except AttributeError:
+                    pass
+        else:
+            raise AttributeError
+
+    def get_area(self):
+        return self.__dict__['_Cylinder__area']
 
 
 a = Cylinder(1, 2)
-print(a.area)
-
-print(a.make_area(2, 2))
+print(a.get_area())
+a.make_area(14, 5)
+print(a.get_area())
+a.diam = 3
+a.high = 3
+print(a.get_area())
+a.make_area(3, 1)
+print(a.get_area())
